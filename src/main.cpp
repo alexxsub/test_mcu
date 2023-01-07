34409171c15891722a26f81d778626539ca0599f
alexx sub <alexei.s@list.ru>
2023-01-07 13:49:23 +0300

    add btn

--- src/main.cpp
+++ src/main.cpp
@@ -5,23 +5,55 @@
 #include <Adafruit_GFX.h>
 #include <Adafruit_SSD1306.h>
 
-
+#include <../lib/button.h>
 
 Adafruit_SSD1306 display = Adafruit_SSD1306(128, 64, &Wire, -1);
-unsigned long delayTime;
+
 
 #define DHTPIN 12
+#define BTNPIN 14
 
 #define DHTTYPE DHT11
 
 
 DHT dht(DHTPIN, DHTTYPE);
-
+button btn1(BTNPIN);
 
 float Temperature;
 float Humidity;
 
+String utf8rus(String source)
+{
+  int i,k;
+  String target;
+  unsigned char n;
+  char m[2] = { '0', '\0' };
 
+  k = source.length(); i = 0;
+
+  while (i < k) {
+    n = source[i]; i++;
+
+    if (n >= 0xC0) {
+      switch (n) {
+        case 0xD0: {
+          n = source[i]; i++;
+          if (n == 0x81) { n = 0xA8; break; }
+          if (n >= 0x90 && n <= 0xBF) n = n + 0x30;
+          break;
+        }
+        case 0xD1: {
+          n = source[i]; i++;
+          if (n == 0x91) { n = 0xB8; break; }
+          if (n >= 0x80 && n <= 0x8F) n = n + 0x70;
+          break;
+        }
+      }
+    }
+    m[0] = n; target = target + String(m);
+  }
+return target;
+}
 
 
 void setup() {
@@ -37,8 +69,12 @@ void setup() {
   display.setTextSize(1.75);
   display.setTextColor(WHITE);
 }
-
+bool flag = false;
+uint32_t btnTimer = 0;
 void loop() {
+
+  if (btn1.click()) Serial.println("press 1");
+
    Humidity = dht.readHumidity();
   // Read temperature as Celsius (the default)
   Temperature = dht.readTemperature();
@@ -54,14 +90,14 @@ void loop() {
   Serial.print(Humidity);
   Serial.print(F("%  Temperature: "));
   Serial.print(Temperature);
-  Serial.print(F("°C "));
+  Serial.println(F("°C "));
 
   display.setCursor(0,0);
   display.clearDisplay();
 
   display.setTextSize(1);
   display.setCursor(0,0);
-  display.print("Temperature: ");
+  display.print(utf8rus("Температура: "));
   display.setTextSize(2);
   display.setCursor(0,10);
   display.print(Temperature);
@@ -75,7 +111,7 @@ void loop() {
   // display humidity
   display.setTextSize(1);
   display.setCursor(0, 35);
-  display.print("Humidity: ");
+  display.print(utf8rus("Влажность: "));
   display.setTextSize(2);
   display.setCursor(0, 45);
   display.print(Humidity);
